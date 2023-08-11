b73059af18e52096d238b1fbf8c7c656b8319bc0
AnupamaRamesh5 <137329019+AnupamaRamesh5@users.noreply.github.com>
2023-08-12 01:01:01 +0530

    Add files via upload

---
+++ learning.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+learnbash="Hello World!"
+echo $learnbash
+provider="Hinger"
+echo 'the best hosting is from $provider'
+echo "the best hosting is from $provider"
+sleep 10 && echo “I’ve been sleeping for 10 seconds, I want more” && sleep 10 && echo “I’m done with sleeping, thanks!”
